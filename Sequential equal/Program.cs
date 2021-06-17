069abb334155f917805d1380214a17ab84e2145d
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 16:57:41 +0530

    Add files via upload

---
+++ Sequential equal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cityList1 = new List<string> { "Delhi", "Mumbai", "Hyderabad" };
+            List<string> cityList2 = new List<string> { "Delhi", "Mumbai", "Hyderabad" };
+            bool IsEqual = cityList1.SequenceEqual(cityList2);
+            Console.WriteLine(IsEqual);
+            Console.ReadLine();
+        }
+    }
+}
